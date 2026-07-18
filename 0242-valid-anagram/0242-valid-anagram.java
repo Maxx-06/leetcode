56d8a22380c158f85e907aca72e63d6085a5c856
Ujjwal Bhardwaj <88519378+Maxx-06@users.noreply.github.com>
2026-07-18 09:43:39 -0700

    Time: 6 ms (43.27%), Space: 44.5 MB (60.55%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,37 +1,64 @@
+// class Solution {
+//     public boolean isAnagram(String s, String t) {
+//         HashMap<Character, Integer> mp = new HashMap<>();
+//         if(s.length() != t.length()){
+//             return false;
+//         }
+//         for(int i=0; i<s.length(); i++){
+//             char ch = s.charAt(i);
+//             if(mp.containsKey(ch)){
+//                 mp.put(ch, mp.get(ch) + 1);
+//             }
+//             else {
+//                 mp.put(ch, 1);
+//             }
+//         }
+
+//          for(int i=0; i<t.length(); i++){
+//             char ch = t.charAt(i);
+//             if(!mp.containsKey(ch)){
+//                 return false;
+//             }
+//             int val = mp.get(ch);
+
+//             if(val == 1){
+//                 mp.remove(ch);
+//             }
+//             else {
+//                 mp.put(ch, mp.get(ch) - 1);
+//             }
+//         }
+
+//         if (mp.size() != 0) {
+//           return false;  
+//          }
+
+//         return true;
+//     }
+// }
+
+// above solution is using hashMap 
+
 class Solution {
     public boolean isAnagram(String s, String t) {
-        HashMap<Character, Integer> mp = new HashMap<>();
+        int[] ans=new int[26];
         if(s.length() != t.length()){
             return false;
         }
-        for(int i=0; i<s.length(); i++){
-            char ch = s.charAt(i);
-            if(mp.containsKey(ch)){
-                mp.put(ch, mp.get(ch) + 1);
-            }
-            else {
-                mp.put(ch, 1);
-            }
+
+        for(int i=0;i<s.length();i++){
+            ans[s.charAt(i) - 'a']++;
         }
 
-         for(int i=0; i<t.length(); i++){
-            char ch = t.charAt(i);
-            if(!mp.containsKey(ch)){
+        for(int j=0;j<t.length();j++){
+            ans[t.charAt(j) - 'a']--;
+        }
+
+        for(int k=0;k<ans.length;k++){
+            if(ans[k]!=0){
                 return false;
             }
-            int val = mp.get(ch);
-
-            if(val == 1){
-                mp.remove(ch);
-            }
-            else {
-                mp.put(ch, mp.get(ch) - 1);
-            }
         }
-
-        if (mp.size() != 0) {
-          return false;  
-         }
 
         return true;
     }
