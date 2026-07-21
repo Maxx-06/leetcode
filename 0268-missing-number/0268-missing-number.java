c7ed09e1408fda043a0e8b49f799bef6c5c50523
Ujjwal Bhardwaj <88519378+Maxx-06@users.noreply.github.com>
2026-07-21 12:45:51 -0700

    Time: 0 ms (100%), Space: 46.9 MB (97.71%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,5 +1,6 @@
 class Solution {
     public int missingNumber(int[] nums) {
+        // sum of n integer 
         // int len = nums.length;
         // int sum = ((len + 1)*(len))/2;
         // for (int i =0; i<len; i++){
@@ -7,6 +8,8 @@ class Solution {
         // }
         // return sum;
 
+
+        // this approach using bitwise xor to eliminate duplicate and left only missing num
         int len = nums.length;
         int res = nums[0];
         for(int i=1; i<len; i++){
