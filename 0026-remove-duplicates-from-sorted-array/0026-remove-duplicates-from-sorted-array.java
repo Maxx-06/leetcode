c53b4246abc132bb478410be1a7f3117dd3d690d
Ujjwal Bhardwaj <88519378+Maxx-06@users.noreply.github.com>
2026-09-20 00:34:29 -0700

    Time: 1 ms (77.28%), Space: 46.7 MB (42.94%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,15 +1,27 @@
-class Solution {
-    public int removeDuplicates(int[] arr) {
-        int l = 1;
+// class Solution {
+//     public int removeDuplicates(int[] arr) {
+//         int l = 1;
 
-        for(int i = 1;i<arr.length; i++){
+//         for(int i = 1;i<arr.length; i++){
+//             if(arr[i] != arr[i-1]){
+//                 arr[l] = arr[i];
+//                 l++;
+//             }
+//         }
+
+//         return l;
+
+//     }
+// }
+class Solution {
+    public int removeDuplicates(int []arr){
+        int l =  1;
+        for(int i=1; i<arr.length; i++){
             if(arr[i] != arr[i-1]){
                 arr[l] = arr[i];
                 l++;
             }
         }
-
         return l;
-
     }
 }
